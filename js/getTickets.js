c53d205ad7487a6e88dcc616c90cacd391482360
ber4bbit <ber4bbit@gmail.com>
2022-10-22 22:42:56 +0700

    added filter/render time back logic and added correction on route duration

--- js/getTickets.js
+++ js/getTickets.js
@@ -1,56 +1,22 @@
 let fromAtoB = [];
 let fromBtoA = [];
 
-const defaultTimeZone = 3;
-const userTimeZone = new Date().getTimezoneOffset() / 60 * -1;
-
-
-
-
-
-
-
-
-const differenceTimeZone = () => {
-    return userTimeZone > defaultTimeZone
-        ? userTimeZone - defaultTimeZone
-        : defaultTimeZone - userTimeZone
-}
-
 const changeTimeZone = (timeArr) => {
-
-    let time = new Date();
-    time.setHours(Math.trunc((timeArr[0].time.replace(/:/g, '') / 100)));
-    time.setMinutes(timeArr[0].time.replace(/:/g, '') % 100);
-
+    const userTz = moment.tz.guess();
+    moment.tz.setDefault('Europe/Moscow');
 
     timeArr.forEach(elem => {
-        let hours = Math.trunc((elem.time.replace(/:/g, '') / 100) + differenceTimeZone());
-        let minutes = elem.time.replace(/:/g, '') % 100;
-
-        if (hours > 24) hours = `0${hours - 24}`
-        else if (hours === 24) hours = `00`
-
-        if (minutes === 0) minutes = '00'
-
-        let userTime = `${hours}:${minutes}`;
-
         let option = document.createElement('option');
-        option.innerText = userTime;
-        
+        option.innerText = moment()
+            .hour(Math.trunc((elem.time.replace(/:/g, '') / 100)))
+            .minute(elem.time.replace(/:/g, '') % 100)
+            .tz(userTz)
+            .format('HH:mm MMMM D');
+
         timeArr[0].time === '18:00'
             ? fromAtoB.push(option)
             : fromBtoA.push(option)
     })
-
-
-    const userTz = moment.tz.guess();
-    let momentTime = moment();
-    momentTime.hour(Math.trunc((timeArr[0].time.replace(/:/g, '') / 100)));
-    momentTime.minute(timeArr[0].time.replace(/:/g, '') % 100);
-    momentTime.tz.setDefault('Europe/Moscow')
-
-    console.log(momentTime.format('HH:mm'));
 }
 
 const getData = async () => {
--- js/script.js
+++ js/script.js
@@ -1,57 +1,63 @@
-const ticketTime = document.getElementById('time');
-const ticketTimeBack = document.getElementById('back-time');
-const ticketRoute = document.getElementById('route');
+const ticketRoute = document.getElementById('ticket-route');
+const ticketTime = document.getElementById('ticket-time');
+const ticketBackTime = document.getElementById('ticket-back-time');
 const defaultRoute = 'Выберите пункт назначения';
 const defaultTime = 'Выберите время';
 const singleRoutePrice = 700;
 const doubleRoutePrice = 1200;
 const singleRouteDuration = 50;
 
-const selectRouteHandle = routeSelect => {
-    addOptions(ticketTime)
-
+// Обработчик, который скрывает select с выбором времени, пока не выбран маршрут и также скрывающий select для выбора времени обратного маршрута, если не выбран соответствующий маршрут
+const ticketTimeSelectHandle = routeSelect => {
     routeSelect.value != defaultRoute
         ? ticketTime.classList.remove('time-select_hidden')
         : ticketTime.classList.add('time-select_hidden');
 
     routeSelect.value === 'из A в B и обратно в А'
-        ? ticketTimeBack.classList.remove('time-select_hidden')
-        : ticketTimeBack.classList.add('time-select_hidden');
+        ? ticketBackTime.classList.remove('time-select_hidden')
+        : ticketBackTime.classList.add('time-select_hidden');
+
+    addOptions(ticketTime)
 }
 
+// Обработчик, который добавляет билеты (времена) в зависимости от маршрута
 const addOptions = timeSelect => {
     if (ticketRoute.value === 'из A в B') {
-        while (timeSelect.children[1]) timeSelect.removeChild(timeSelect.children[1])
+        while (timeSelect.children[1]) timeSelect.removeChild(timeSelect.children[1]) // Удаление всех option`ов из select, кроме дефолтного
         timeSelect.append(...fromAtoB);
     } else if (ticketRoute.value === 'из B в A') {
         while (timeSelect.children[1]) timeSelect.removeChild(timeSelect.children[1])
         timeSelect.append(...fromBtoA);
     } else if (ticketRoute.value === 'из A в B и обратно в А') {
         while (timeSelect.children[1]) timeSelect.removeChild(timeSelect.children[1])
-        timeSelect.append(...fromBtoA);
+        timeSelect.append(...fromAtoB);
     }
 }
 
 const renderBackTime = timeSelect => {
+    while (ticketBackTime.children[1]) ticketBackTime.removeChild(ticketBackTime.children[1])
+
     let timeBackOptionsArr = [];
     let elemClone;
 
-    while (ticketTimeBack.children[1]) ticketTimeBack.removeChild(ticketTimeBack.children[1])
-
-    //TODO: Надо сделать сравнение с учетом смены часового пояса
+    // Создание объекта moment из value селекта для дальнейшего сравнения каждого элемента массива с временами с ним
+    let timeSelectValueMomentObj = new moment()
+            .date(timeSelect.value.replace(/[:' 'A-Za-z]/g, '') % 100)
+            .hour(Math.trunc(timeSelect.value.replace(/[:' 'A-Za-z]/g, '') / 10000))
+            // Прибавляем время singleRouteDuration в пути, чтобы правильно отфильтровать время, которое доступно для отправления обратно, потому что нельзя ведь отправиться в 22:30
+            // из A в B, а затем в 22:45 отправиться обратно из B в A
+            .minute((Math.trunc(timeSelect.value.replace(/[:' 'A-Za-z]/g, '') / 100) % 100) + singleRouteDuration);
 
     fromBtoA.forEach(elem => {
-        elem.value > timeSelect.value
-            ? (
-                elemClone = elem.cloneNode(true),
-                timeBackOptionsArr.push(elemClone)
-            )
-            : null
+
+        // Создание объекта moment из каждого элемента массива с временами
+        let elemMomentObj = new moment()
+            .date(elem.value.replace(/[:' 'A-Za-z]/g, '') % 100)
+            .hour(Math.trunc(elem.value.replace(/[:' 'A-Za-z]/g, '') / 10000))
+            .minute(Math.trunc(elem.value.replace(/[:' 'A-Za-z]/g, '') / 100) % 100); 
+
+        // Метод isAfter проверяет идет ли дата (объект), на котором вызван метод после даты, которая передается в качестве параметра isAfter
+        if (elemMomentObj.isAfter(timeSelectValueMomentObj)) elemClone = elem.cloneNode(true); timeBackOptionsArr.push(elemClone);
     })
-
-    ticketTimeBack.append(...timeBackOptionsArr);
-}
-
-const selectTimeHandle = () => {
-    // filterTime(ticketTime)
+    ticketBackTime.append(...timeBackOptionsArr);
 }
